75d91be60b61584a901dbac37f1a9e065ba76061
Ryan Kopping <ryko006@gmail.com>
2024-04-14 11:50:21 +0300

    Updating functions Exercise XP

---
+++ Week 2/Day 3/ExerciseXP/Exercise_XP_Gold.py
@@ -0,0 +1,42 @@
+# Write a function called get_full_name() that takes three arguments: 1: first_name, 2: middle_name 3: last_name.
+# middle_name should be optional, if it’s omitted by the user, the name returned should only contain the first and the last name.
+# For example, get_full_name(first_name="john", middle_name="hooker", last_name="lee") will return John Hooker Lee.
+# But get_full_name(first_name="bruce", last_name="lee") will return Bruce Lee.
+
+def get_full_name (first_name,last_name,**kwargs):
+    middle_name = kwargs.get("middle_name","")
+    return (f"{first_name} {middle_name} {last_name}")
+
+print(get_full_name (first_name="ryan",last_name="kopping"))
+
+
+# Write a function that converts English text to morse code and another one that does the opposite.
+# Hint: Check the internet for a translation table, every letter is separated with a space and every word is separated with a slash /.
+
+morse =           { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    encrypt_message=""
+    for letter in message:
+        if letter != " ":
+            encrypt_message+=morse[letter.upper()]+" "
+        else:
+            encrypt_message+= " "
+    return (encrypt_message)
+
+
+print(encrypt ("test"))
--- Week 2/Day 3/ExerciseXP/Exercises_XP.py
+++ Week 2/Day 3/ExerciseXP/Exercises_XP.py
@@ -1,33 +1,33 @@
 # Create a function called get_random_temp().
 # This function should return an integer between -10 and 40 degrees (Celsius), selected at random.
 # Test your function to make sure it generates expected results.
-# import random
-# def get_random_temp(season):
-#     if season.lower() =="winter":
-#         return random.randint(-10,16)
-#     elif season.lower() == "autumn":
-#         return random.randint(16,23)
-#     elif season.lower() == "spring":
-#         return random.randint(23,32)
-#     elif season.lower() == "summer":
-#         return random.randint(32,40)
+import random
+def get_random_temp(season):
+    if season.lower() =="winter":
+        return random.randint(-10,16)
+    elif season.lower() == "autumn":
+        return random.randint(16,23)
+    elif season.lower() == "spring":
+        return random.randint(23,32)
+    elif season.lower() == "summer":
+        return random.randint(32,40)
 
 
-# def main():
-#     season = input("Enter a season: Summer, Winter, Autumn or Spring")
-#     temp = get_random_temp(season)
-#     if 0>temp:
-#         print ("Brr, it's freezing today")
-#     elif 0<temp<16:
-#         print("It's not freezing but definitely take a coat")
-#     elif 16<temp<23:
-#         print("Temps are warming up, you'll need a light sweater today")
-#     elif 24<temp<32:
-#         print("Buckle up, we're entering summer temps!")
-#     else:
-#         print("Stay in the shade today, it's cooking out there!")
+def main():
+    season = input("Enter a season: Summer, Winter, Autumn or Spring")
+    temp = get_random_temp(season)
+    if 0>temp:
+        print ("Brr, it's freezing today")
+    elif 0<temp<16:
+        print("It's not freezing but definitely take a coat")
+    elif 16<temp<23:
+        print("Temps are warming up, you'll need a light sweater today")
+    elif 24<temp<32:
+        print("Buckle up, we're entering summer temps!")
+    else:
+        print("Stay in the shade today, it's cooking out there!")
 
-# main()
+main()
 
 #Ex.2
 
@@ -36,66 +36,66 @@
 # Bonus : display to the user the questions he answered wrong, his answer, and the correct answer.
 # If he had more then 3 wrong answers, ask him to play again.
 
-# data = [
-#     {
-#         "question": "What is Baby Yoda's real name?",
-#         "answer": "Grogu"
-#     },
-#     {
-#         "question": "Where did Obi-Wan take Luke after his birth?",
-#         "answer": "Tatooine"
-#     },
-#     {
-#         "question": "What year did the first Star Wars movie come out?",
-#         "answer": "1977"
-#     },
-#     {
-#         "question": "Who built C-3PO?",
-#         "answer": "Anakin Skywalker"
-#     },
-#     {
-#         "question": "Anakin Skywalker grew up to be who?",
-#         "answer": "Darth Vader"
-#     },
-#     {
-#         "question": "What species is Chewbacca?",
-#         "answer": "Wookiee"
-#     }
-# ]
+data = [
+    {
+        "question": "What is Baby Yoda's real name?",
+        "answer": "Grogu"
+    },
+    {
+        "question": "Where did Obi-Wan take Luke after his birth?",
+        "answer": "Tatooine"
+    },
+    {
+        "question": "What year did the first Star Wars movie come out?",
+        "answer": "1977"
+    },
+    {
+        "question": "Who built C-3PO?",
+        "answer": "Anakin Skywalker"
+    },
+    {
+        "question": "Anakin Skywalker grew up to be who?",
+        "answer": "Darth Vader"
+    },
+    {
+        "question": "What species is Chewbacca?",
+        "answer": "Wookiee"
+    }
+]
 
-# def ask_question (info_list):
-#     wrong_answers=[]
-#     for dict in info_list:
-#         print (dict["question"])
-#         answer = input()
-#         if answer == dict["answer"]:
-#             print ("Correct!")
-#         else:
-#             print("Incorrect")
-#             print (dict["answer"])
-#             wrong_answers.append({"question":dict['question'],"Your Answer":[answer],"Right Answer":dict["answer"]})
+def ask_question (info_list):
+    wrong_answers=[]
+    for dict in info_list:
+        print (dict["question"])
+        answer = input()
+        if answer == dict["answer"]:
+            print ("Correct!")
+        else:
+            print("Incorrect")
+            print (dict["answer"])
+            wrong_answers.append({"question":dict['question'],"Your Answer":[answer],"Right Answer":dict["answer"]})
 
-#     if len(wrong_answers)>3:
-#         play_again = input("Do you want to play again? Y/N")
-#         if play_again.lower()=="Y":
-#             ask_question(data)
-#         else:
-#             print("Thanks for playing")
+    if len(wrong_answers)>3:
+        play_again = input("Do you want to play again? Y/N")
+        if play_again.lower()=="Y":
+            ask_question(data)
+        else:
+            print("Thanks for playing")
 
-#     return(wrong_answers)
+    return(wrong_answers)
 
-# def check_score(wrong_answers):
-#     total_questions = len(data)
-#     correct_answers = total_questions-len(wrong_answers)
-#     print (f"You got {correct_answers} correct and {len(wrong_answers)} wrong.\nThese are the questions you got wrong, the correct answer and your answer")
-#     for answer in wrong_answers:
-#         print(f"Question: {answer['question']}")
-#         print(f"Your Answer: {answer['Your Answer']}")
-#         print(f"Correct Answer: {answer['Right Answer']}")
-#         print() 
+def check_score(wrong_answers):
+    total_questions = len(data)
+    correct_answers = total_questions-len(wrong_answers)
+    print (f"You got {correct_answers} correct and {len(wrong_answers)} wrong.\nThese are the questions you got wrong, the correct answer and your answer")
+    for answer in wrong_answers:
+        print(f"Question: {answer['question']}")
+        print(f"Your Answer: {answer['Your Answer']}")
+        print(f"Correct Answer: {answer['Right Answer']}")
+        print() 
 
 
-# check_score(ask_question(data))
+check_score(ask_question(data))
 
 # The point of the exercise is to check if a person can retire depending on their age and their gender.
 # Note : Let’s say retirement age is 67 for men, and 62 for women (born after April, 1947).
@@ -116,26 +116,26 @@
 # Display a message informing the user whether they can retire or not.
 
 
-# def get_age (year,month,day):
-#     current_year=2024
-#     current_month="April"
-#     age=current_year-year
-#     return (age)
+def get_age (year,month,day):
+    current_year=2024
+    current_month="April"
+    age=current_year-year
+    return (age)
 
-# def can_retire(gender,date_of_birth):
-#     date_of_birth = date_of_birth.split("/")
-#     year=int(date_of_birth[0])
-#     month=int(date_of_birth[1])
-#     day=int(date_of_birth[2])
-#     user_age=get_age(year,month,day)
-#     if gender.lower()=='m' and 67<=user_age or gender.lower()=='f' and 62<=user_age:
-#         return ("Can retire")
-#     else:
-#         return ("Cannot retire")
+def can_retire(gender,date_of_birth):
+    date_of_birth = date_of_birth.split("/")
+    year=int(date_of_birth[0])
+    month=int(date_of_birth[1])
+    day=int(date_of_birth[2])
+    user_age=get_age(year,month,day)
+    if gender.lower()=='m' and 67<=user_age or gender.lower()=='f' and 62<=user_age:
+        return ("Can retire")
+    else:
+        return ("Cannot retire")
     
 
-# gender,birthdate = map(str,input ("Enter a gender and a birthdate (yyyy/mm/dd)\n").split(" "))
-# print(can_retire (gender, birthdate))
+gender,birthdate = map(str,input ("Enter a gender and a birthdate (yyyy/mm/dd)\n").split(" "))
+print(can_retire (gender, birthdate))
 
 # Exercise 4:
 # Instructions
