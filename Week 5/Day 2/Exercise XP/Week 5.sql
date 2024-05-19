19906407f33f69ef9aa9f57c735c4567d33322c9
Ryan Kopping <ryko006@gmail.com>
2024-05-19 11:21:14 -0400

    update

---
+++ Week 5/Day 2/Exercise XP/Week 5.sql
@@ -0,0 +1,24 @@
+-- select *
+-- from items;
+-- select *
+-- from customers;
+
+-- select *
+-- from items
+-- where price>80
+
+-- select *
+-- from items
+-- where price<=300
+
+-- select *
+-- from customers
+-- where last_name like 'Smith'
+
+-- select *
+-- from customers
+-- where last_name like 'Jones'
+
+select *
+from customers
+where first_name not like 'Scott'
